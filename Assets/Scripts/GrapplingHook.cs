0f7d17fb9f2b1dac043d710801a688c0c9179df6
Andreas Reich <r_andreas2@web.de>
2019-10-20 22:57:19 +0200

    Basic GrapplingHook

---
+++ Assets/Scripts/GrapplingHook.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GrapplingHook : MonoBehaviour
+{
+    enum State
+    {
+        Idle,
+        MovingToTarget,
+        Retracting
+    }
+
+    private State state = State.Idle;
+
+    private Vector3 target;
+
+    public float MoveSpeed = 10.0f;
+
+    void FixedUpdate()
+    {
+        if (state == State.Idle)
+            return;
+
+        var toTarget = target - transform.position;
+        float distance = toTarget.magnitude;
+        if (distance < 0.02f)
+        {
+            if (state == State.MovingToTarget)
+            {
+                target = transform.parent.position;
+                state = State.Retracting;
+            }
+            else
+            {
+                state = State.Idle;
+                return;
+            }
+        }
+
+        float movement = Mathf.Min(1.0f, MoveSpeed * Time.fixedDeltaTime / distance);
+        transform.Translate(toTarget * movement);
+    }
+
+    public void OnFire()
+    {
+        if (state != State.Idle)
+            return;
+
+        var mousePos = Mouse.current.position.ReadValue();
+        target = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y));
+        state = State.MovingToTarget;
+    }
+}
---
+++ Assets/Scripts/GrapplingHookRope.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+[ExecuteInEditMode]
+public class GrapplingHookRope : MonoBehaviour
+{
+    void Update()
+    {
+        GetComponent<LineRenderer>().SetPositions(new Vector3[] {
+            new Vector3(transform.position.x, transform.position.y, 1.0f),
+            new Vector3(transform.parent.position.x, transform.parent.position.y, 1.0f)
+        });
+    }
+}
