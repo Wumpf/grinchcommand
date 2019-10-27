5b6b9fe51c666493e56846a16891eee56ead0cd2
Andreas Reich <r_andreas2@web.de>
2019-10-27 16:42:54 +0100

    destroyed hooktower sprite

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -16,9 +16,6 @@ public class Building : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (HappySprite == null)
-            gameObject.SetActive(false);
-
         GetComponent<BoxCollider2D>().enabled = false;
 
         var spriteRenderer = GetComponent<SpriteRenderer>();
@@ -30,11 +27,17 @@ public class Building : MonoBehaviour
             light.enabled = true;
         if (LooseLifeIfHitByPresent)
             OnLifeLostEvent?.Invoke();
+
+        for (int i=0; i<transform.childCount; ++i)
+            transform.GetChild(i).gameObject.SetActive(false);
     }
 
     public void Restart()
     {
         gameObject.SetActive(true);
+        for (int i=0; i<transform.childCount; ++i)
+            transform.GetChild(i).gameObject.SetActive(true);
+
         GetComponent<BoxCollider2D>().enabled = true;
         if (normalSprite != null)
             GetComponent<SpriteRenderer>().sprite = normalSprite;
